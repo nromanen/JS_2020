bc29d39992137f5800829d45aa65f4704a4c9508
Mykhailo Meleshchuk <dyugran@gmail.com>
2020-03-19 17:52:48 +0200

    init: add file with JSON parse

---
+++ server2.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (req, res) {
+
+    if (req.url === "/") {
+        fs.readFile("./public/index.html", "UTF-8", function (err, html) {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(html);
+        });
+    } else if (req.url.match("\.css$")) {
+        var cssPath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(cssPath, "UTF-8");
+        res.writeHead(200, { "Content-Type": "text/css" });
+        fileStream.pipe(res);
+
+    } else if (req.url.match("\.png$")) {
+        var imagePath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, { "Content-Type": "image/png" });
+        fileStream.pipe(res);
+    } else if (req.url.match("\.json$")) {
+        fs.readFile("user.json", function (err, json) {
+            var obj = JSON.parse(json);
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(`<h1 style="color: red">Hello ${obj[0].username}<h1>`)
+        });
+    } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("No Page Found");
+    }
+
+}).listen(3000);
